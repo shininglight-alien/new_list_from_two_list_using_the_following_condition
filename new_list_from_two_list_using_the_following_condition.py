c449496062d8a9d825c8d2e0eb1f258021ec7a0f
Ellaine Rivera <ellainejoycerrivera@iskolarngbayan.pup.edu.ph>
2024-01-25 12:54:00 +0800

    created list

--- new_list_from_two_list_using_the_following_condition.py
+++ new_list_from_two_list_using_the_following_condition.py
@@ -3,3 +3,6 @@
 def merge_lists(list1, list2):
     new_list = [num for num in list1 if num % 2 != 0] + [num for num in list2 if num % 2 == 0]
     return new_list
+
+list1 = [10, 20, 25, 30, 35]
+list2 = [40, 45, 60, 75, 90]
